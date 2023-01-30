e5763ed20f76fbd7e1d33d910ecb4749de25fbf5
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-01-30 21:28:35 +0530

    added strict port

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  port: {
-    strictPort: 3000,
+  server: {
+    port: 3000,
+    strictPort: true,
   },
 });
