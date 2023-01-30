dba8ae499654c0ef41dc92d1aca851f347387dc3
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-01-30 21:25:30 +0530

    added body parser

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.APP_PORT || 4001;
 
