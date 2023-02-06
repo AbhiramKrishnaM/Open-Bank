d7dcc614289968760ccb7a55315e5139a222aa6f
Harikrishnan <hknp796@gmail.com>
2023-02-06 19:04:43 +0530

    remaining fundings card

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import RemainingFunds from "./components/RemainingFunds";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const one = {
+    amount: "200",
+    days: "15",
+  };
 
   return (
     <div className="App">
-      <div className="bg-red-600">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RemainingFunds data={one} />
     </div>
   );
 }
